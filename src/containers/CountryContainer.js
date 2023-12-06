6e704a8fad8b13ebd5c239f1c036a7943ee3c8a4
Emily Reid <emily.reid31@gmail.com>
2023-12-06 20:21:06 +0000

    added useState to set countries array, added fetchCountry function and useEffect hook to get country data from the API

--- src/containers/CountryContainer.js
+++ src/containers/CountryContainer.js
@@ -1,6 +1,22 @@
-import { useState, UseEffect } from "react";
+import { useState, useEffect } from "react";
 
 const CountryContainer = () => {
+    // useState to make an empty array (null), will create the array of countries once first country fetch request is sent
+    const [countries, setCountries] = useState([]);
+
+    // async fetches country data (promise)
+    const fetchCountry = async () => {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const data = await response.json();
+        // update data state
+        setCountries(data);
+
+    }
+    // useEffect hook to get country data with fetchCountry
+    useEffect(() => {
+        fetchCountry();
+    },[]) 
+
     return(
         <h1>Country Bucket List</h1>
     )
